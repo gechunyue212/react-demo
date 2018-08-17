dcbc7d1321582be6140fb01f856d2a19ee7bb1f4
yuchen <you@example.com>
2018-08-17 17:49:18 +0800

    样式微调

--- src/App.js
+++ src/App.js
@@ -57,11 +57,11 @@ class App extends Component {
         <ChartArea>
             <KChart contentText={kChart[language]}/>
         </ChartArea>
+          <div id="CoreAdvantages" style={{height:"100px"}} />
+          <BrBox title={product[language].title} />
+          <div style={{height:"85px"}} />
+          <ProductList contentText={product[language]} />
           <SkillArea>
-              <div id="CoreAdvantages" style={{height:"100px"}} />
-              <BrBox title={product[language].title} />
-              <div style={{height:"85px"}} />
-              <ProductList contentText={product[language]} />
               <div style={{height:"46px"}} />
               <BrBox title={technology[language].title} />
               <div style={{height:"64px"}} />
