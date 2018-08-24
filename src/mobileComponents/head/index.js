a47ce7ae39a72fa688e309bcd15b72e4f47edb44
gechunyue <gechunyue@echoex.io>
2018-08-24 17:39:53 +0800

    解决遮罩层下内容滚动问题

--- src/mobileComponents/head/index.js
+++ src/mobileComponents/head/index.js
@@ -3,7 +3,7 @@ import styles from './index.scss';
 import HeadLogo from '../../assets/mobileImg/head_logo.png';
 import NavBtn from '../../assets/mobileImg/head_nav.png';
 import MavBtnBgImg from '../../assets/mobileImg/right_nav_bg.png';
-
+var rootdiv = document.getElementsByTagName("body")[0];
 export default class MobileHead extends Component{
 
     constructor(props){
@@ -19,6 +19,11 @@ export default class MobileHead extends Component{
     toggleOpenNav = ()=>{
         const { navStatus } = this.state;
         this.setState({ navStatus:!navStatus });
+        if( navStatus == false){
+            rootdiv.style.cssText = "height:100%;overflow:hidden;position:fixed"
+        }else{
+            rootdiv.style.cssText="height:100%;overflow:auto"
+        }
         
     };
 
@@ -31,6 +36,11 @@ export default class MobileHead extends Component{
         // if(typeof this.props.onClick === 'function'){
             this.props.handleMenuClick(value);
         // }
+        if( navStatus == false){
+            rootdiv.style.cssText = "height:100%;overflow:hidden;position:fixed"
+        }else{
+            rootdiv.style.cssText="height:100%;overflow:auto"
+        }
     }
     render(){
 
@@ -43,7 +53,7 @@ export default class MobileHead extends Component{
                 <div onClick={this.toggleOpenNav} className={styles.headBtn}>
                     <img src={NavBtn} />
                 </div>
-                <div className={styles.rightNavBox + " " + (navStatus ? styles.showNav : styles.hideNav)}>
+                <div className={styles.rightNavBox + " " + (navStatus ? styles.showNav : styles.hideNav)} >
                     <div className={styles.rightNavBgColor + " " + (navStatus ? styles.rightNavBgColorShow : styles.rightNavBgColorHide)}/>
                     <div className={styles.rightNav + " " + (navStatus ? styles.rightNavShow : styles.rightNavHide)}>
                         <div className={styles.rightNavBg}>
