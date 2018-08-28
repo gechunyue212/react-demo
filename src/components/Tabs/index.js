68c283127c77040a8a3201eafbeab337cc726670
gechunyue <gechunyue@echoex.io>
2018-08-28 19:48:32 +0800

    table组件功能实现

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -4,7 +4,9 @@ export default class Tabs extends Component {
     constructor(props){
         super(props);
         this.state={
-            current:"MARKED"
+            contentText:props.contentText,
+            current:"USDT",
+            choosedArr:[]
         }
     }
     changebg = (current) =>{
@@ -12,35 +14,124 @@ export default class Tabs extends Component {
             current
         })
     }
+
+    choose = (option,index) =>{
+        let { contentText } = this.state;
+        const { choosedArr } = this.state
+        contentText[option].tbody[index].active = !contentText[option].tbody[index].active;
+        console.log(contentText[option][index]);
+        let tempArr = [];
+        tempArr.push(contentText[option][index]);
+            this.setState({
+                contentText,
+                choosedArr:tempArr
+            })
+    }
     render(){
         const { contentText } = this.props
-        const { current } = this.state;
+        const { current,choosedArr } = this.state;
         return(
             <div className={s.TabsBox}>
                 <ul className={s.Tabs}>
-                    <li className={current === "MARKED" ? s.active : "" } onClick={()=>this.changebg("MARKED")}><span className={s.bg}></span>{contentText.head}</li>
+                    <li className={current === "MARKED" ? s.active : "" } onClick={()=>this.changebg("MARKED")}><span className={s.bg}></span>{contentText.tab}</li>
                     <li className={current === "USDT" ? s.active : "" } onClick={()=>this.changebg("USDT")}>USDT</li>
                     <li className={current === "BTC" ? s.active : "" } onClick={()=>this.changebg("BTC")}>BTC</li>
                     <li className={current === "ETH" ? s.active : "" } onClick={()=>this.changebg('ETH')}>ETH</li>
                 </ul>
                 <table>
                      <tbody>
-                    {
-                      contentText.table.map((item,i)=>(  
-                        <tr key={i}>
+                         <tr>
                             {
-                                item.tr.map((p,j)=>(
-                                    p.td.indexOf("/") == -1?<td key={j}>{p.td}</td>:
-                                    <td key={j}><span className={s.star}></span><span style={{color:'#93A4DA'}}>{p.td.split("/")[0]}</span><span style={{color:'#605280'}}>/{p.td.split("/")[1]}</span></td>
+                                
+                                contentText.thead.map((item,i)=>(
+                                    <td key={i}>{item}</td>
                                 ))
                             }
-                        </tr>
-                        
-                      ))  
-                    }
+                         </tr>
+                            {
+                                contentText[current].tbody.map((item,i)=>(
+                                    <tr key={i}>
+                                        {
+                                            item.tr.map((p,j)=>(
+                                                p.td.indexOf("/") == -1?<td key={j}>{p.td}</td>:
+                                                <td key={j}><span className={s.star+" "+(item.active === true?s.nocheck:s.check)} onClick={()=>this.choose(current, i)}></span><span style={{color:'#93A4DA'}}>{p.td.split("/")[0]}</span><span style={{color:'#605280'}}>/{p.td.split("/")[1]}</span></td>
+                                            ))
+                                        }
+                                    </tr>
+                                ))
+                            }
                     </tbody>
                 </table>
             </div>
         )
     }
 }
+
+
+
+
+
+
+
+// {
+//     current === "MARKED" ?
+//    <span>
+//       <tr>
+//            {contentText.table.tr.map((item,j)=>(
+//                   <td key={j}>{item.td}</td>
+//               ))}
+//        </tr>
+//         {choosedArr.map((item,i)=>(  
+//            <tr key={i} className={s.choosed}>
+//                {
+//                    item.tr.map((p,j)=>(
+//                        p.td.indexOf("/") == -1?<td key={j}>{p.td}</td>:
+//                        <td key={j}><span className={s.star}></span><span style={{color:'#93A4DA'}}>{p.td.split("/")[0]}</span><span style={{color:'#605280'}}>/{p.td.split("/")[1]}</span></td>
+//                    ))
+//                }
+//            </tr>
+          
+//           ))}  
+//       </span>
+//       :
+//       current === "USDT" ?
+//       contentText.table1.map((item,i)=>(  
+//           <tr key={i}>
+//               {
+//                   item.tr.map((p,j)=>(
+//                       p.td.indexOf("/") == -1?<td key={j}>{p.td}</td>:
+//                       <td key={j}><span className={s.star+" "+(item.active === true?s.nocheck:s.check)} onClick={()=>this.choose("table1", i)}></span><span style={{color:'#93A4DA'}}>{p.td.split("/")[0]}</span><span style={{color:'#605280'}}>/{p.td.split("/")[1]}</span></td>
+//                   ))
+//               }
+//           </tr>
+          
+//       )) 
+//       :
+//       current === "BTC" ?
+//       contentText.table2.map((item,i)=>(  
+//           <tr key={i}>
+//               {
+//                   item.tr.map((p,j)=>(
+//                       p.td.indexOf("/") == -1?<td key={j}>{p.td}</td>:
+//                       <td key={j}><span className={s.star+" "+(item.active === true?s.nocheck:s.check)} onClick={()=>this.choose("table2", i)}></span><span style={{color:'#93A4DA'}}>{p.td.split("/")[0]}</span><span style={{color:'#605280'}}>/{p.td.split("/")[1]}</span></td>
+//                   ))
+//               }
+//           </tr>
+          
+//       ))
+//       :
+//       current === "ETH" ?
+//       contentText.table3.map((item,i)=>(  
+//           <tr key={i}>
+//               {
+//                   item.tr.map((p,j)=>(
+//                       p.td.indexOf("/") == -1?<td key={j}>{p.td}</td>:
+//                       <td key={j}><span className={s.star+" "+(item.active === true?s.nocheck:s.check)} onClick={()=>this.choose("table3", i)}></span><span style={{color:'#93A4DA'}}>{p.td.split("/")[0]}</span><span style={{color:'#605280'}}>/{p.td.split("/")[1]}</span></td>
+//                   ))
+//               }
+//           </tr>
+          
+//       ))
+//       :
+//       ''
+//   }
