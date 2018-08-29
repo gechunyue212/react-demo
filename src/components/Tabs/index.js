19eabb3a70ff7008d8aebb5ca9da08964534e5e0
gechunyue <gechunyue@echoex.io>
2018-08-29 11:27:16 +0800

    代码优化

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -32,9 +32,8 @@ export default class Tabs extends Component {
         })
     }
     render(){
-        const { contentText1 } = this.state;
+        const { contentText1,current } = this.state;
         const { contentText } = this.props;
-        const { current } = this.state;
         return(
             <div className={s.TabsBox}>
                 <ul className={s.Tabs}>
@@ -81,65 +80,3 @@ export default class Tabs extends Component {
 
 
 
-// {
-//     current === "MARKED" ?
-//    <span>
-//       <tr>
-//            {contentText.table.tr.map((item,j)=>(
-//                   <td key={j}>{item.td}</td>
-//               ))}
-//        </tr>
-//         {choosedArr.map((item,i)=>(  
-//            <tr key={i} className={s.choosed}>
-//                {
-//                    item.tr.map((p,j)=>(
-//                        p.td.indexOf("/") == -1?<td key={j}>{p.td}</td>:
-//                        <td key={j}><span className={s.star}></span><span style={{color:'#93A4DA'}}>{p.td.split("/")[0]}</span><span style={{color:'#605280'}}>/{p.td.split("/")[1]}</span></td>
-//                    ))
-//                }
-//            </tr>
-          
-//           ))}  
-//       </span>
-//       :
-//       current === "USDT" ?
-//       contentText.table1.map((item,i)=>(  
-//           <tr key={i}>
-//               {
-//                   item.tr.map((p,j)=>(
-//                       p.td.indexOf("/") == -1?<td key={j}>{p.td}</td>:
-//                       <td key={j}><span className={s.star+" "+(item.active === true?s.nocheck:s.check)} onClick={()=>this.choose("table1", i)}></span><span style={{color:'#93A4DA'}}>{p.td.split("/")[0]}</span><span style={{color:'#605280'}}>/{p.td.split("/")[1]}</span></td>
-//                   ))
-//               }
-//           </tr>
-          
-//       )) 
-//       :
-//       current === "BTC" ?
-//       contentText.table2.map((item,i)=>(  
-//           <tr key={i}>
-//               {
-//                   item.tr.map((p,j)=>(
-//                       p.td.indexOf("/") == -1?<td key={j}>{p.td}</td>:
-//                       <td key={j}><span className={s.star+" "+(item.active === true?s.nocheck:s.check)} onClick={()=>this.choose("table2", i)}></span><span style={{color:'#93A4DA'}}>{p.td.split("/")[0]}</span><span style={{color:'#605280'}}>/{p.td.split("/")[1]}</span></td>
-//                   ))
-//               }
-//           </tr>
-          
-//       ))
-//       :
-//       current === "ETH" ?
-//       contentText.table3.map((item,i)=>(  
-//           <tr key={i}>
-//               {
-//                   item.tr.map((p,j)=>(
-//                       p.td.indexOf("/") == -1?<td key={j}>{p.td}</td>:
-//                       <td key={j}><span className={s.star+" "+(item.active === true?s.nocheck:s.check)} onClick={()=>this.choose("table3", i)}></span><span style={{color:'#93A4DA'}}>{p.td.split("/")[0]}</span><span style={{color:'#605280'}}>/{p.td.split("/")[1]}</span></td>
-//                   ))
-//               }
-//           </tr>
-          
-//       ))
-//       :
-//       ''
-//   }
