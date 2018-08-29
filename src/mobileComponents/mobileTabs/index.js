610d5b7f125baea027684badb4015c7910eb0bb9
gechunyue <gechunyue@echoex.io>
2018-08-29 17:18:20 +0800

    手机端table组件功能

--- src/mobileComponents/mobileTabs/index.js
+++ src/mobileComponents/mobileTabs/index.js
@@ -13,18 +13,36 @@ export default class MobileTabs extends Component{
             current
         })
     }
-    componentWillReceiveProps(nextProps){
+    choose = (option,index) =>{
+        let { contentText } = this.state;
+        let temp = contentText.tab
+        contentText[option].tbody[index].active = !contentText[option].tbody[index].active;
+        if(contentText[option].tbody[index].active === true){
+            contentText[temp].tbody.push(contentText[option].tbody[index]);
+        }else{
+            contentText[temp].tbody.forEach((element,index) => {
+                if(element.active === false){
+                    contentText[temp].tbody.splice(index,1);
+                }
+            });
+        }
         this.setState({
-            contentText:nextProps.contentText 
+            contentText,
+        })
+    }
+    componentWillReceiveProps(nextProps){
+        console.log(nextProps);
+        this.setState({
+            contentText:nextProps.contentText,
+            current:nextProps.contentText.tab
         })
     }
     render(){
-        const { contentText } = this.state
-        const { current } = this.state;
+        const { current,contentText } = this.state;
         return(
             <div className={s.TabsBox}>
                 <ul className={s.Tabs}>
-                    <li className={current === "MARKED" ? s.active : "" } onClick={()=>this.changebg("MARKED")}><span className={s.bg}></span>{contentText.head}</li>
+                    <li className={current === "MARKED" ? s.active : "" } onClick={()=>this.changebg("MARKED")}><span className={s.bg}></span>{contentText.tab}</li>
                     <li className={current === "USDT" ? s.active : "" } onClick={()=>this.changebg("USDT")}>USDT</li>
                     <li className={current === "BTC" ? s.active : "" } onClick={()=>this.changebg("BTC")}>BTC</li>
                     <li className={current === "ETH" ? s.active : "" } onClick={()=>this.changebg('ETH')}>ETH</li>
@@ -32,17 +50,20 @@ export default class MobileTabs extends Component{
                 <table>
                      <tbody>
                     {
-                      contentText.table.map((item,i)=>(  
-                        <tr key={i}>
+                    contentText[current].tbody.length!=0?
+                      contentText[current].tbody.map((item,i)=>(  
+                        <tr key={i} onClick={()=>this.choose(current,i)}>
                             {
                                 item.tr.map((p,j)=>(
                                     p.td[0].indexOf("/") == -1?(p.td.length == 1?<td key={j}><span className={s.btn}>{p.td[0]}</span></td>:<td key={j}><span>{p.td[0]}</span><br/><span>{p.td[1]}</span></td>):
-                                    <td key={j}><span className={s.star}></span><div style={{marginTop:'0.22rem'}}><span style={{color:'#93A4DA',fontSize:'0.24rem'}}>{p.td[0].split("/")[0]}</span><span style={{color:'#605280',fontSize:'0.24rem'}}>/{p.td[0].split("/")[1]}</span><br/><span>{p.td[1]}</span></div></td>
+                                    <td key={j}><span className={s.star + " "+(item.active===false?s.nocheck:s.check)}></span><div style={{marginTop:'0.22rem'}}><span style={{color:'#93A4DA',fontSize:'0.24rem'}}>{p.td[0].split("/")[0]}</span><span style={{color:'#605280',fontSize:'0.24rem'}}>/{p.td[0].split("/")[1]}</span><br/><span>{p.td[1]}</span></div></td>
                                 ))
                             }
                         </tr>
                         
-                      ))  
+                      ))
+                      :
+                      <tr><td style={{textAlign:'center'}}>暂无记录</td><td></td><td></td></tr> 
                     }
                     </tbody>
                 </table>
